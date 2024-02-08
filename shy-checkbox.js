97c5df6548ae00b15d553f0fd16a3501f16714df
Tim White <walton.timothy.james.white@gmail.com>
2024-02-08 20:49:38 +1300

    Kinda moves following the mouse cursor, but offset

--- shy-checkbox.js
+++ shy-checkbox.js
@@ -1,5 +1,22 @@
 console.log("Hello world from shy-checkbox.js!");
 
+checkbox = document.getElementById("chkbox");
+console.log(`checkbox=${checkbox}.`);   //DEBUG
+body = document.getElementById("body");
+console.log(`body=${body}.`);   //DEBUG
+
+body.addEventListener("mousemove", moveCheckboxToCursor);
+
 function doSomething(e) {
-    e.style.position = 'fixed'; e.style.left = 0; e.style.top = 0;
+    moveTo(e, { screenX: 250, screenY: 150 });
+}
+
+function moveCheckboxToCursor(e) {
+    moveTo(checkbox, e);
+}
+
+function moveTo(o, event) {
+    o.style.position = 'fixed';
+    o.style.left = event.screenX + "px";
+    o.style.top = event.screenY + "px";
 }
